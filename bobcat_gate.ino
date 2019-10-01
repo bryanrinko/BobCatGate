40692513c19a3ed244a5dbda4ce6d44a5d5a88a7
Bryan Rinko <bryan.rinko@gmail.com>
2019-10-01 07:22:00 -0400

    Update to support 2 gates
    The code is optimize to support 2 gates.  Means removing delays in the code to minimize impact to either timing gate and simulate parallel  processing.  Also added libraries to support multi OLED displays over the same bus.

--- bobcat_gate.ino
+++ bobcat_gate.ino
@@ -1,23 +1,33 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Multi_BitBang.h>
+#include <Multi_OLED.h>
+//https://github.com/bitbank2/Multi_BitBang
+//https://github.com/bitbank2/Multi_OLED
+
 #include <SoftwareSerial.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+#define NUM_DISPLAYS 2
+#define NUM_BUSES 2
+// I2C bus info
+uint8_t scl_list[NUM_BUSES] = {0xb5,0xb5}; //{9,9};
+uint8_t sda_list[NUM_BUSES] = {0xc6, 0xd7}; //{5,6};
+int32_t speed_list[NUM_BUSES] = {400000L, 400000L};
+// OLED display info
+uint8_t bus_list[NUM_DISPLAYS] = {1,1}; // can be multiple displays per bus
+uint8_t addr_list[NUM_DISPLAYS] = {0x3c, 0x3d};
+uint8_t type_list[NUM_DISPLAYS] = {OLED_128x64, OLED_128x64};
+uint8_t flip_list[NUM_DISPLAYS] = {0,0};
+uint8_t invert_list[NUM_DISPLAYS] = {0,0};
 
 SoftwareSerial mySerial(0, 1); // RX, TX
 
-const int GATERELAY=7;
+const int GATE1RELAY=7;
+const int GATE2RELAY=8;
+
 const int OPENSTATE=1;
-const String SENSORUP = "up:";
 const int debounce = 250; // debounce latency in ms
 const int endDelay = 3000;
 
-char *myLoopStates[] = {"INIT", "Ready", "Starting","Started", "Ending", "Ended"};
+char *myLoopStates[] = {"sensorINIT", "Ready", "Starting","Started", "Ending", "Ended"};
 const int SENSORINIT = 0;
 const int READY = 1;
 const int STARTING = 2;
@@ -25,97 +35,85 @@ const int STARTED = 3;
 const int ENDING = 4;
 const int ENDED = 5;
 
-char *myBrand[] = {"BOBgate", "v0.06"};
+char *myBrand[] = {"BOBgate", "v0.07"};
 const int DEVICE_BRAND = 0;
 const int VERSION = 1;
 
 int flag = 0;
-String state;
+char *state[] = {"",""};
 
-unsigned long StartTime = 0;
-unsigned long EndTime = 0;
-unsigned long ElapsedTime = 0;
+unsigned long StartTime[] = {0,0};
+unsigned long EndTime[] = {0,0};
+unsigned long ElapsedTime[] = {0,0};
 
 void setup() {
   // Start Serial
   Serial.begin(9600);
+
+  Multi_I2CInit(sda_list, scl_list, speed_list, NUM_BUSES);
+  Multi_OLEDInit(bus_list, addr_list, type_list, flip_list, invert_list, NUM_DISPLAYS);
   
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-  display.clearDisplay();
-  
-  myOLED(myBrand[DEVICE_BRAND],"Loading",myBrand[VERSION]);
+  myOLED2Gate(myBrand[DEVICE_BRAND],-1, "Loading",myBrand[VERSION]);
   Serial.println(myBrand[DEVICE_BRAND]);
   Serial.println("Loading");
   Serial.println(myBrand[VERSION]);
   delay(2000);
 
-  pinMode(GATERELAY, INPUT_PULLUP);
-  state = myLoopStates[SENSORINIT];
+  pinMode(GATE1RELAY, INPUT_PULLUP);
+  pinMode(GATE2RELAY, INPUT_PULLUP);
+
+  state[0] = myLoopStates[SENSORINIT];
+  state[1] = myLoopStates[SENSORINIT];
 }
 
 void loop() {
-  runLoop();
+   evalState(0,GATE1RELAY);
+   evalState(1,GATE2RELAY);
 }
 
-void runLoop(){
-   int val = digitalRead(GATERELAY);
-
-   if (state == myLoopStates[SENSORINIT] && val == OPENSTATE){
-      state = myLoopStates[READY];
-      myOLED(myBrand[DEVICE_BRAND],state,"");
-      Serial.println(state);
+void evalState(int gateIndex,int gatePin){
+   int val = digitalRead(gatePin);
+   
+   if (state[gateIndex] == myLoopStates[SENSORINIT] && val == OPENSTATE){
+      state[gateIndex] = myLoopStates[READY];
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1, state[gateIndex], "");
    }else{
-    if (state == myLoopStates[READY] && val != OPENSTATE){
-      state = myLoopStates[STARTING];
-      StartTime = millis();
-      myOLED(myBrand[DEVICE_BRAND],state,"");
-      Serial.println(state);
-      delay(debounce);
-    } else if(state == myLoopStates[STARTING] && val == OPENSTATE){
-      state = myLoopStates[STARTED];
-      myOLED(myBrand[DEVICE_BRAND],state,"");
-      Serial.println(state);
-    } else if(state == myLoopStates[STARTED] && val != OPENSTATE){
-      state = myLoopStates[ENDING];
-      EndTime = millis();
-      myOLED(myBrand[DEVICE_BRAND],state,"");
-      Serial.println(state);
-      delay(debounce);
-    } else if(state == myLoopStates[ENDING] && val == OPENSTATE){
-      state = myLoopStates[ENDED];
-      ElapsedTime = EndTime - StartTime;
-      String finalTime = String(ElapsedTime / 1000.0, 3);
-      myOLED(myBrand[DEVICE_BRAND],state,finalTime);
-      Serial.println(finalTime);
-      delay(endDelay);
-    } else if(state == myLoopStates[ENDED] && val == OPENSTATE){
-      state = myLoopStates[READY];
-      myOLED(myBrand[DEVICE_BRAND],state,"");
-      Serial.println(state);
+    if (state[gateIndex] == myLoopStates[READY] && val != OPENSTATE){
+      state[gateIndex] = myLoopStates[STARTING];
+      StartTime[gateIndex] = millis();
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1, state[gateIndex], "");
+    } else if(state[gateIndex] == myLoopStates[STARTING] && val == OPENSTATE && millis()>= StartTime[gateIndex]+debounce){
+      state[gateIndex] = myLoopStates[STARTED];
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1, state[gateIndex], "");
+    } else if(state[gateIndex] == myLoopStates[STARTED] && val != OPENSTATE){
+      state[gateIndex] = myLoopStates[ENDING];
+      EndTime[gateIndex] = millis();
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1, state[gateIndex], "");
+    } else if(state[gateIndex] == myLoopStates[ENDING] && val == OPENSTATE  && millis()>= EndTime[gateIndex]+debounce){
+      state[gateIndex] = myLoopStates[ENDED];
+      ElapsedTime[gateIndex] = EndTime[gateIndex] - StartTime[gateIndex];
+      String finalTime = String(ElapsedTime[gateIndex] / 1000.0, 3);
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1, state[gateIndex], finalTime.c_str());
+    } else if(state[gateIndex] == myLoopStates[ENDED] && val == OPENSTATE && millis()>= EndTime[gateIndex]+endDelay){
+      state[gateIndex] = myLoopStates[READY];
+      myOLED2Gate(myBrand[DEVICE_BRAND],gateIndex+1,state[gateIndex], "");
     }
   }
 }
 
-void myOLED(String header, String line1, String line2) {
-  display.clearDisplay();
+void myOLED2Gate(char *header, int gate, char *lineText1, char *lineText2) {
+  Serial.print("GATE:");
+  Serial.print(gate);
+  Serial.print(" -> ");
+  if (gate == 1){
+     Serial.println(lineText1);
+  } else if (gate == 2){
+     Serial.println(lineText2);
+  }
   
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 1);
-  display.println(header);
-
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10, 18);
-  display.println(line1);
-
-  display.setTextSize(3);
-  display.setTextColor(WHITE);
-  display.setCursor(10, 40);
-  display.println(line2);
-  
-  display.display();
+  //Clear Display
+  Multi_OLEDFill(gate-1, 0);
+  Multi_OLEDWriteString(gate-1, 0, 1, (char *)header, FONT_SMALL, 0);
+  Multi_OLEDWriteString(gate-1, 10, 18, (char *)lineText1, FONT_NORMAL, 0);
+  Multi_OLEDWriteString(gate-1, 10, 40, (char *)lineText2, FONT_LARGE, 0);
 }
